c90f24e4d383727c93013fb68b6f54038d7e369b
Akshay Kumar <akshay020602@gmail.com>
2025-05-20 17:08:31 +0000

    add new file 01 variable .js  for all variable information

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,17 @@
+const accountId=14414009;
+let userMail='akshay020602@gmail.com';
+var userPassword='Akshay@2025';
+userCity='Delhi';
+
+//accountId=14900078; //Not Allowrd
+/*
+    prefer not to use var
+    because of issue in block scope and functional scope.
+*/
+userMail='ak@2023.com';
+userPassword='Kumar@2022';
+userCity='Sasaram';
+console.table([accountId, userMail, userPassword, userCity]);
+
+console.log(accountId);
+
