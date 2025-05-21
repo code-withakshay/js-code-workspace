d73aeea148b916da21e5ad6ceeed167fd703daaf
Akshay Kumar <akshay020602@gmail.com>
2025-05-21 05:12:38 +0000

    Data Type class notes and practice

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,22 @@
+"use strict" //treat all JS code as newer version.
+
+//alert("Do not click here...!"); //we are using node.js, not browuer
+
+let age=20; //integer type
+let  name='Akshay'; // string type
+let isLoginId=false; // boolean type
+let status; //value is undefined
+
+//number => 2 to power 53
+//bigint
+//String=""
+//boolean => true/false
+//null => standlone value
+//undefined =>
+//symbol => unique
+
+//Object
+
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
