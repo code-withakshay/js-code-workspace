72fb2e07da13e9953c9a0968d5ea99556506536d
Akshay Kumar <akshay020602@gmail.com>
2025-05-22 12:58:28 +0000

    Operation

--- 01_basic/03_convertionOperation.js
+++ 01_basic/03_convertionOperation.js
@@ -1,10 +1,10 @@
 let scroe="Akshay"
-console.log(typeof scroe);
-console.log(typeof(scroe));
+// console.log(typeof scroe);
+// console.log(typeof(scroe));
 
 let valueNumber = Number(scroe);
-console.log(typeof valueNumber );
-console.log(valueNumber);
+// console.log(typeof valueNumber );
+// console.log(valueNumber);
 
 //"33" => 33
 //"33abc" => NaN (Not a Number)
@@ -13,7 +13,7 @@ console.log(valueNumber);
 let isLoggedIn="Akshay"
 
 let booleanIsLonggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLonggedIn);
+//console.log(booleanIsLonggedIn);
 
 //1 => true, 0 => false
 // "" => false
@@ -23,7 +23,45 @@ let someNumber=33
 
 let stringNumber= String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+//########################### OPERATION #############################
+
+let value=3
+
+let negValue=-value
+
+// console.log(negValue); //-3
+
+// console.log(2+2); // 4
+// console.log(2-2); // 0
+// console.log(2*2); // 4
+// console.log(2/2); // 1
+// console.log(2**3); // 8
+// console.log(2%3); // 2
+
+let str1='Hello';
+let str2=' Akshay';
+let str3=str1+str2;
+
+//console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+
+// console.log(2 + 3 * 4 % 5); // 4  {(3*4) = 12 (12%5=2) (2+2=4)}
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3
+num1=num2=num3=2+2;
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter);
