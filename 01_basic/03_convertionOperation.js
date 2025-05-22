66002ccfd463fb724598b1ff689e5020f0558371
Akshay Kumar <akshay020602@gmail.com>
2025-05-22 11:50:39 +0000

    convertion of data type

---
+++ 01_basic/03_convertionOperation.js
@@ -0,0 +1,29 @@
+let scroe="Akshay"
+console.log(typeof scroe);
+console.log(typeof(scroe));
+
+let valueNumber = Number(scroe);
+console.log(typeof valueNumber );
+console.log(valueNumber);
+
+//"33" => 33
+//"33abc" => NaN (Not a Number)
+//true => 1, false => 0
+
+let isLoggedIn="Akshay"
+
+let booleanIsLonggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLonggedIn);
+
+//1 => true, 0 => false
+// "" => false
+//"Akshay" =>true
+
+let someNumber=33
+
+let stringNumber= String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
